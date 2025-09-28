b841a56674dcf338469ad5a377344b1c5cb2e485
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-28 20:30:59 +0100

    task 3 complete

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,10 +1,4 @@
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
-
+import { ButtonProps } from "@/interfaces";
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
